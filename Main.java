9c5d5e4da84c68968678658b0a0be1ce74655e77
Tyler Mcdowell <tyler.mcdowell@patelhs.org>
2020-10-30 14:20:13 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) 
+  {
+      String[ ] names = {"Tyler", "Marcelo", "Destiny", "Mateo", "Sofia"};
+
+      for (int i = 0; i < names.length; i++)
+     {
+      System.out.println( names[i] );
+     }
+   }
+}
